e0bf7f10797e3a5b35d72ed8d4d9aa52d5788f91
YakoobHammouri <jakoub-h2002@hotmail.com>
2023-09-21 14:13:12 +0300

    first commit

---
+++ files/search/text_position[6].js
@@ -0,0 +1,1 @@
+positionForPages[5]={"page":6,"positions":[{"w":"\u00fe\u00b1\u0003\u00cf\u00b1\u00bb\u0003\u0097\u0003dfi=gR\u0013\u00b1\u0003R7\u00ea\u00b1","p":[-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,0.017750,0.004437,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan,-nan]}]};
